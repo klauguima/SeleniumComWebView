f0d15c9cdfc411edf548522491d312b2d872aae6
klauguima <klauguima@gmail.com>
2020-03-23 17:58:49 -0300

    Curso de webDriver com Selenium - até aula 35.

---
+++ CursoSelenium/src/main/java/TesteAlert.java
@@ -0,0 +1,62 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteAlert {
+	@Test
+	public void deveInteragirComAlertSimples() {
+		System.setProperty("webdriver.chrome.driver", "C:/WebDrivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("alert")).click();
+		Alert alert = driver.switchTo().alert();
+		String texto = alert.getText();
+		Assert.assertEquals("Alert Simples", texto);
+		alert.accept();
+		driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
+	}
+
+	@Test
+	public void deveInteragirComAlertConfirm() {
+		System.setProperty("webdriver.chrome.driver", "C:/WebDrivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("confirm")).click();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alerta.getText());
+		alerta.accept();
+		Assert.assertEquals("Confirmado", alerta.getText());
+		alerta.accept();
+		driver.findElement(By.id("confirm")).click();
+		alerta = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alerta.getText());
+		alerta.dismiss();
+		Assert.assertEquals("Negado", alerta.getText());
+		alerta.dismiss();
+		driver.quit();
+	}
+
+	@Test
+	public void deveInteragirComAlertPrompt() {
+		System.setProperty("webdriver.chrome.driver", "C:/WebDrivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("prompt")).click();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Digite um numero", alerta.getText());
+		alerta.sendKeys("12");
+		alerta.accept();
+		Assert.assertEquals("Era 12?", alerta.getText());
+		alerta.accept();
+		Assert.assertEquals(":D", alerta.getText());
+		alerta.accept();
+		driver.quit();
+	}
+}
--- CursoSelenium/src/main/java/TesteGoogle.java
+++ CursoSelenium/src/main/java/TesteGoogle.java
@@ -1,7 +1,6 @@
-import java.awt.Dimension;
-
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,16 +8,11 @@ public class TesteGoogle {
 	
 	@Test
 	public void teste(){
-//		System.setProperty("webdriver.chrome.driver", "C:/WebDrivers/geckodriver.exe");
-//		System.setProperty("webdriver.chrome.driver", "C:/WebDrivers/IEDriverServer.exe");
-//		WebDriver driver = new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver", "C:/WebDrivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-//		driver.manage().window().setPosition(new Point(100, 100));
 		driver.manage().window().setSize(new Dimension(1200, 765));
 		driver.get("http://www.google.com");
 		Assert.assertEquals("Google", driver.getTitle());
-		
+		driver.quit();
 	}
-
 }
